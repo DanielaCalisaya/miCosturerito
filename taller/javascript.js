f8c1709a2c58ed8e8bb9f52ce507caa16bbc9e58
DanielaCalisaya <hdaniela127c@gmail.com>
2023-03-19 21:41:32 -0300

    completed carousel and added photos

--- taller/javascript.js
+++ taller/javascript.js
@@ -24,3 +24,35 @@
 /*      * Esta es una landing page, no redirige a otras páginas, por eso al llevarnos a otra parte de la misma página
           debe remover todo el menu y la x y mostrar la hamburguesa 
 */
+
+
+/* carousel------------ */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
